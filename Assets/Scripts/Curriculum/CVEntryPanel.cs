848585fa9339365d5a5d1d8c67ac38ff288c5223
PCristian00 <cristianp9300@gmail.com>
2024-05-13 15:23:52 +0200

    Array di inputField
    Introdotto array di inputField in CVEntryPanel per rendere il codice più versatile.

--- Assets/Scripts/Curriculum/CVEntryPanel.cs
+++ Assets/Scripts/Curriculum/CVEntryPanel.cs
@@ -3,24 +3,47 @@ using UnityEngine;
 
 public class CVEntryPanel : MonoBehaviour
 {
-    // FARE ARRAY DI INPUTFIELD?
 
-    public TMP_InputField nameInputField;
-    public TMP_InputField surnameInputField;
-    public TMP_InputField jobInputField;
+    public TMP_InputField[] InputFields;
+
+    private bool allClear = true;
 
 
     public void Submit()
     {
-        if (AcceptInput(nameInputField) && AcceptInput(surnameInputField) && AcceptInput(jobInputField))
+        foreach (TMP_InputField inputField in InputFields)
         {
-            CVEntry CV = new(nameInputField.text, surnameInputField.text, jobInputField.text);
+            if (!AcceptInput(inputField))
+            {
+                Debug.Log("Campo inesistente");
+
+                inputField.image.color = Color.red;
+
+                allClear = false;
+
+                // Rimettere break riduce i controlli ma non fa colorare di rosso tutti i campi (solo il primo non valido)
+                // break;
+            }
+
+            else inputField.image.color = Color.white;
+
+
+        }
+
+        if (allClear)
+        {
+            CVEntry CV = new(InputFields[0].text, InputFields[1].text, InputFields[2].text);
             CVManager.AddCVEntry(CV);
         }
-        else Debug.Log("INSERIRE TUTTI I CAMPI");
-    }
 
-    // INSERIRE CONTROLLO CAMPI VUOTI, NON COMPATIBILI...
+
+        //if (AcceptInput(nameInputField) && AcceptInput(surnameInputField) && AcceptInput(jobInputField))
+        //{
+        //    CVEntry CV = new(nameInputField.text, surnameInputField.text, jobInputField.text);
+        //    CVManager.AddCVEntry(CV);
+        //}
+        //else Debug.Log("INSERIRE TUTTI I CAMPI");
+    }
 
     public bool AcceptInput(TMP_InputField inputField)
     {
