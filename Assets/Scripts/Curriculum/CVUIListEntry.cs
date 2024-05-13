8215648a4b881767f77142e575f7d984d90ec4c5
PCristian00 <cristianp9300@gmail.com>
2024-05-13 13:08:01 +0200

    Lista CV selezionabili (Bozza)
    Inserimento di Button nella lista dei CV che permette di sceglierne uno per future operazioni.
    Completare OnEntrySelect per implementazione.

--- Assets/Scripts/Curriculum/CVUIListEntry.cs
+++ Assets/Scripts/Curriculum/CVUIListEntry.cs
@@ -5,10 +5,19 @@ public class CVUIListEntry : MonoBehaviour
 {
     public TextMeshProUGUI nameText;
     public TextMeshProUGUI surnameText;
+    private CVEntry CVEntry;
         
     public void Setup(CVEntry entry)
     {
+        CVEntry = entry;
         nameText.text = entry.name;
         surnameText.text = entry.surname;
+    }
+
+    public void OnEntrySelect()
+    {
+        // TEST
+        // QUI VA INSERITA FUNZIONE CHE AVVIA SCENA DEL GIOCO CORRELATA AL CVENTRY SCELTO
+        CVManager.DebugCV(CVEntry);
     }
 }
