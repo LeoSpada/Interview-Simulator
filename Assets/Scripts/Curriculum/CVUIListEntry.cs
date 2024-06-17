d747de55e8bec4f514aa08aa02e58680a36fb0c2
PCristian00 <cristianp9300@gmail.com>
2024-06-17 17:11:31 +0200

    Corretto errore CVEntry
    Rimesso codice rimosso erroneamente.

--- Assets/Scripts/Curriculum/CVUIListEntry.cs
+++ Assets/Scripts/Curriculum/CVUIListEntry.cs
@@ -1,37 +1,43 @@
+using TMPro;
 using UnityEngine;
 
 // Gestisce le funzioni del singolo elemento della lista dei CV.
 public class CVUIListEntry : MonoBehaviour
 {
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI surnameText;
+
     private CVEntry CVEntry;
     public GameObject deleteConfirmPanel;
 
     public void Setup(CVEntry entry)
     {
         CVEntry = entry;
+        nameText.text = entry.name;
+        surnameText.text = entry.surname;
     }
 
     public void LoadEntryScene()
-    {        
-        CVManager.currentCV = CVEntry;        
+    {
+        CVManager.currentCV = CVEntry;
         GameManager.instance.LoadScene("Scena_Colloquio");
     }
 
     public void EditEntry()
-    {        
+    {
         CVManager.editCurrent = true;
-        CVManager.currentCV = CVEntry;        
+        CVManager.currentCV = CVEntry;
         GameManager.instance.LoadScene("Scena_Curriculum");
     }
 
     public void DeleteEntry()
-    {        
-        CVManager.RemoveCVEntry(CVEntry);        
-        GameManager.instance.LoadScene("Scena_Lista_CV");        
+    {
+        CVManager.RemoveCVEntry(CVEntry);
+        GameManager.instance.LoadScene("Scena_Lista_CV");
     }
 
     public void DeleteButton()
-    {       
+    {
         deleteConfirmPanel.SetActive(true);
     }
 }
